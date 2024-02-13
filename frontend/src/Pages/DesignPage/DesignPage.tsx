c752035c92c3576907b21949b613bcb156a25672
vlad64 <vladok.malashuk@gmail.com>
2024-02-13 12:59:32 +0100

    Added DesignPage and Table

---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Table from '../../Components/Table/Table'
+
+interface Props {}
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+    <h1>Finshark Design Page</h1>
+    <h2>This is Finshark's design page.</h2>
+    <Table/>
+    </>
+  )
+}
+
+export default DesignPage
