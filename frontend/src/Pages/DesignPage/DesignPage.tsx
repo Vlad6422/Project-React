cd1693ad87b91c1e681c75c41b1060511415c419
vlad64 <vladok.malashuk@gmail.com>
2024-02-13 21:57:44 +0100

    Company Profile/TTM

--- frontend/src/Pages/DesignPage/DesignPage.tsx
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import Table from '../../Components/Table/Table'
 import RatioList from '../../Components/RatioList/RatioList'
+import { testIncomeStatementData } from '../../Components/Table/testData'
 
 interface Props {}
-
+const tableConfig = [
+  {
+    label: "Market Cap",
+    render: (company: any) => company.marketCapTTM,
+    subTitle: "Total value of all a company's shares of stock",
+  }
+]
 const DesignPage = (props: Props) => {
   return (
     <>
     <h1>Finshark Design Page</h1>
     <h2>This is Finshark's design page.</h2>
-    <RatioList/>
+    <RatioList data={testIncomeStatementData} config ={tableConfig}/>
     <Table/>
     </>
   )
