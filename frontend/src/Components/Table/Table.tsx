d8a234df9cca188ecb9a9bfb53d141f0627dfaee
Vlad6422 <vladok.malashuk@gmail.com>
2024-02-25 13:38:20 +0100

    Changed : Table design

--- frontend/src/Components/Table/Table.tsx
+++ frontend/src/Components/Table/Table.tsx
@@ -20,7 +20,7 @@ const Table = ({config,data}: Props) => {
     });
     const renderedHeaders = config.map((config: any) => {
         return (
-            <th className="p-4 text-left text-xs font-medium text-fray-500 uppercase tracking-wider"
+            <th className="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 key={config.label}
             >
                 {config.label}
@@ -28,8 +28,8 @@ const Table = ({config,data}: Props) => {
         )
     })
     return (<div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
-        <table>
-            <thead className="min-w-full divide-y divide=gray-200 m-5">
+        <table className="min-w-full divide-y divide-gray-200 m-5">
+            <thead className="bg-gray-50">
                 {renderedHeaders}
             </thead>
             <tbody>{renderedRows}</tbody>
